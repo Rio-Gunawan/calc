abe0101b078ca2bba478626f7f1ae7f4cd871fe9
Rio <rio.motoki44@gmail.com>
2021-10-16 11:22:15 +0900

    ESLintの導入

--- calc.js
+++ calc.js
@@ -5,96 +5,101 @@ let buttonHtml = "";
 let mode = [];
 let floatBool = false;
 $(function () {
-	const $output = $('#answer');
-	$output.prepend('<div id="cursor"></div>');
-	$output.prepend(0);
-	const cursorTiming = 500;
-	setInterval(function () {
-		$('#cursor').css('visibility',
-			$('#cursor').css('visibility') == 'hidden' ? 'visible' : 'hidden');
-	}, cursorTiming);
+    const $output = $('#answer');
+    $output.prepend('<div id="cursor"></div>');
+    $output.prepend(0);
+    const cursorTiming = 500;
+    setInterval(function () {
+        $('#cursor').css('visibility',
+            $('#cursor').css('visibility') == 'hidden' ? 'visible' : 'hidden');
+    }, cursorTiming);
 });
 
+// eslint-disable-next-line no-unused-vars
 function clickNum(num) {
-	if (num == '.') {
-		let inputStringVersion = String(answer[answerNum]);
-		if (answer[answerNum] == 0) {
-			inputStringVersion = '0.';
-			num = '0.'
-		} else {
-			inputStringVersion += '.';
-		}
-		floatBool = true;
-		inputStrings += String(num);
-		answer[answerNum] = inputStringVersion;
-		showInputStrings();
-	} else {
-		if (floatBool) {
-			answer[answerNum] += String(num);
-			inputStrings += String(num);
-			showInputStrings();
-		} else {
-			answer[answerNum] = answer[answerNum] * 10 + num;
-			inputStrings += String(num);
-			showInputStrings();
-		}
-	}
+    if (num == '.') {
+        let inputStringVersion = String(answer[answerNum]);
+        if (answer[answerNum] == 0) {
+            inputStringVersion = '0.';
+            num = '0.';
+        } else {
+            inputStringVersion += '.';
+        }
+        floatBool = true;
+        inputStrings += String(num);
+        answer[answerNum] = inputStringVersion;
+        showInputStrings();
+    } else {
+        if (floatBool) {
+            answer[answerNum] += String(num);
+            inputStrings += String(num);
+            showInputStrings();
+        } else {
+            answer[answerNum] = answer[answerNum] * 10 + num;
+            inputStrings += String(num);
+            showInputStrings();
+        }
+    }
 }
 
 function showInputStrings() {
-	const $output = $('#answer');
-	const $buttons = $('#buttons');
-	$buttons.append(buttonHtml)
-	$output.html(inputStrings);
-	$output.append('<div id="cursor"></div>');
+    const $output = $('#answer');
+    const $buttons = $('#buttons');
+    $buttons.append(buttonHtml);
+    $output.html(inputStrings);
+    $output.append('<div id="cursor"></div>');
 }
 
+// eslint-disable-next-line no-unused-vars
 function clearText() {
-	answer = [0];
-	inputStrings = "";
-	answerNum = 0;
-	const $output = $('#answer');
-	$output.text(0);
-	$output.append('<div id="cursor"></div>');
+    answer = [0];
+    inputStrings = "";
+    answerNum = 0;
+    const $output = $('#answer');
+    $output.text(0);
+    $output.append('<div id="cursor"></div>');
 }
 
+// eslint-disable-next-line no-unused-vars
 function showAnswer() {
-	if (floatBool) {
-		answer[answerNum] = parseFloat(answer[answerNum]);
-		floatBool = false;
-	}
-	let localAnswer = answer.shift();
-	let forNum = 0;
-	answer.forEach(num => {
-		if (mode[forNum] == '+') {
-			localAnswer += num;
-		} else if (mode[forNum] == '-') {
-			localAnswer -= num;
-		} else if (mode[forNum] == '*') {
-			localAnswer *= num
-		} else if (mode[forNum] == '/') {
-			localAnswer /= num;
-		}
-		forNum++;
-	});
-	const $output = $('#answer');
-	$output.text(localAnswer);
-	$output.append('<div id="cursor"></div>');
+    if (floatBool) {
+        answer[answerNum] = parseFloat(answer[answerNum]);
+        floatBool = false;
+    }
+    let localAnswer = answer.shift();
+    let forNum = 0;
+    answer.forEach(num => {
+        if (mode[forNum] == '+') {
+            localAnswer += num;
+        } else if (mode[forNum] == '-') {
+            localAnswer -= num;
+        } else if (mode[forNum] == '*') {
+            localAnswer *= num;
+        } else if (mode[forNum] == '/') {
+            localAnswer /= num;
+        }
+        forNum++;
+    });
+    const $output = $('#answer');
+    $output.text(localAnswer);
+    $output.append('<div id="cursor"></div>');
 }
 
+// eslint-disable-next-line no-unused-vars
 function reset() {
-	answer = [0];
-	inputStrings = "";
+    answer = [0];
+    inputStrings = "";
 }
 
+// eslint-disable-next-line no-unused-vars
 function calcMode(modeLocal) {
-	if (floatBool) {
-		answer[answerNum] = parseFloat(answer[answerNum]);
-		floatBool = false;
-	}
-	mode[answerNum] = modeLocal;
-	answerNum++;
-	answer[answerNum] = 0;
-	inputStrings += modeLocal;
-	showInputStrings();
+    if (floatBool) {
+        answer[answerNum] = parseFloat(answer[answerNum]);
+        floatBool = false;
+    }
+    mode[answerNum] = modeLocal;
+    answerNum++;
+    answer[answerNum] = 0;
+    inputStrings += modeLocal;
+    showInputStrings();
 }
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // for scroll
 $(function () {
-	$('a[href^="#"]').click(function () {
-		//スピード
-		let speed = 500;
-		//移動すると指定された場所
-		let href = $(this).attr("href");
-		//実際に移動する場所
-		let target = $(href == "#" || href == "" ? 'html' : href);
-		//その場所までの距離
-		let position = target.offset().top;
-		//アニメーションスタート
-		$("html, body").animate({ scrollTop: position }, speed, "swing");
-		//リンクが作動しないように
-		return false;
-	});
+    $('a[href^="#"]').click(function () {
+        //スピード
+        let speed = 500;
+        //移動すると指定された場所
+        let href = $(this).attr("href");
+        //実際に移動する場所
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        //その場所までの距離
+        let position = target.offset().top;
+        //アニメーションスタート
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        //リンクが作動しないように
+        return false;
+    });
 });
